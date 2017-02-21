ae1da4c763189a22958dfbc0e648040f7d6e79e4
Peep Pullerits <peep@transferwise.com>
2017-02-21 18:16:17 +0200

    Add state-of-the-art build system based on Bash

---
+++ compile.sh
@@ -0,0 +1,2 @@
+javac -cp ./h2-1.3.153.jar:./lwjgl.jar:./lwjgl_util.jar:./slick-util.jar src/ee/peeppullerits/karting/*.java
+
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Djava.library.path=./native/macosx/ -cp lwjgl.jar:h2-1.3.153.jar:lwjgl_util.jar:slick-util.jar:lwjgl-jawt-3.1.1.jar:./src ee.peeppullerits.karting.Main
